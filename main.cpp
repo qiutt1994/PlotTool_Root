c18fc671f8b8e2d2ff1555ba1c7088a733a86c17
Tong Qiu <tong.qiu@outlook.com>
2019-04-23 16:52:23 +0100

    split string

--- main.cpp
+++ main.cpp
@@ -3,21 +3,41 @@
 #include <TH1.h>
 #include <TFile.h>
 #include "region.h"
+#include <sstream>
 using namespace std;
+
+vector<string> split(string input, char splitor)
+{
+	vector<string> output;
+	std::istringstream ss(input);
+	std::string token;
+	while (std::getline(ss, token, '_'))
+	{
+		output.push_back(token);
+	}
+	return output;
+}
 
 int main()
 {
-    std::vector<std::string> sample_list = {)
+    std::vector<std::string> sample_list = {"W", "Wl", "Wcl", "Wbl", "Wbb", "Wbc", "Wcc", "WZ", "WW", "Zcc", "Zcl", "Zbl", "Zbc", "Zl", "Zbb", "Z", "ZZ", "stopWt", "stops", "stopt", "ttbar"};
     sample_list = {"hist-ZmumuL_Sh221.root"};
     TFile *f1 = new TFile("hadd_2lep_mc16d_produced_by_gitlab-CI.root","OPEN");
     //TFile *o1 = (TFile *)f1->Get("Systematics;1");
 
-    for(auto k : *o1->GetListOfKeys()) {
+		std::vector<std::string> vec = {};
+    for(auto k : *f1->GetListOfKeys()) {
         TH1F *hist;
         //hist = (TH1F *) o1->Get(k->GetName());
-        std::string s = k->GetName();
-        std::string token = s.substr(0, s.find("_"));
+        vector<string> sub = split(k->GetName(),'_');
+				if(std::find(vec.begin(), vec.end(), sub[0]) == vec.end())
+				{
+					  vec.push_back(sub[0]);
+				}
     }
+		for(auto i:vec){
+			  cout <<"\""<<i<<"\" ";
+		}
     cout << endl;
     return 0;
 }
